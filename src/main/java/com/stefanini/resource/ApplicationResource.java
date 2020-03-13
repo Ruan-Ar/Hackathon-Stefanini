70c03cac5a627a6bd303a0fab74bd5896a2d924d
Ruan-Ar <ruantratos@gmail.com.br>
2020-03-13 03:06:55 -0300

    adicionado criteriabuilder que busca UF

--- src/main/java/com/stefanini/resource/ApplicationResource.java
+++ src/main/java/com/stefanini/resource/ApplicationResource.java
@@ -1,9 +1,31 @@
 package com.stefanini.resource;
 
+
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("api")
-public class ApplicationResource extends Application{
+import io.swagger.annotations.Api;
+import io.swagger.jaxrs.config.BeanConfig;
 
-}
+@ApplicationPath("api")
+@Api
+public class ApplicationResource extends Application{
+	
+//inicio de configurações do Swagger
+	
+	public ApplicationResource() {
+	    BeanConfig beanConfig = new BeanConfig();
+	    beanConfig.setVersion("1.0");
+	    beanConfig.setSchemes(new String[]{"http"});
+	    beanConfig.setHost("localhost:8080");
+	    beanConfig.setBasePath("/api");
+	    beanConfig.setResourcePackage("com.stefanini.resource");
+	    beanConfig.setScan(true);
+	  }
+	
+	
+	}
+
+
+
